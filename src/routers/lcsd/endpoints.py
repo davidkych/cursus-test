0de2ce3ee7b35423702e4a21016d91489b6ae956
Healthopilot <davidkych.med@gmail.com>
2025-07-13 22:05:46 +0800

    lcsd availability revert

--- src/routers/lcsd/endpoints.py
+++ src/routers/lcsd/endpoints.py
@@ -8,12 +8,6 @@ from .lcsd_af_adminupload_html import router as _admin_html_router
 from .lcsd_af_adminupload_logic import router as _admin_logic_router
 # NOTE: lcsd_cleanup_validator removed per 2025-07 change-request
 from .lcsd_cleanup_validator_scheduler import router as _cleanup_sched_router  # sched version kept
-# availability + HTML helper
-from .availability_endpoints import router as _availability_router
-from .html_availability_endpoints import router as _html_avail_router
-# NEW – HTML timetable form & dashboard
-from .html_timetable_endpoints import router as _html_timetable_router
-from .html_dashboard_endpoints import router as _dashboard_router
 
 router = APIRouter()
 
@@ -24,9 +18,3 @@ router.include_router(_admin_html_router)
 router.include_router(_admin_logic_router)
 # cleanup-validator endpoint intentionally **not** included any longer
 router.include_router(_cleanup_sched_router)
-# availability routes
-router.include_router(_availability_router)
-router.include_router(_html_avail_router)
-# NEW – make HTML UI routes reachable
-router.include_router(_html_timetable_router)
-router.include_router(_dashboard_router)
