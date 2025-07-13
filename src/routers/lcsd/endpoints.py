8c2577648ccde3228e0b1f204b1507873cb89796
Healthopilot <davidkych.med@gmail.com>
2025-07-13 14:44:35 +0800

    lcsd_cleanup_validator new

--- src/routers/lcsd/endpoints.py
+++ src/routers/lcsd/endpoints.py
@@ -6,6 +6,7 @@ from .lcsd_af_timetable import router as _af_tt_router
 from .lcsd_af_excel_timetable import router as _af_excel_router
 from .lcsd_af_adminupload_html import router as _admin_html_router          # NEW
 from .lcsd_af_adminupload_logic import router as _admin_logic_router        # NEW
+from .lcsd_cleanup_validator import router as _cleanup_validator_router     # NEW
 
 router = APIRouter()
 router.include_router(_af_info_router)
@@ -13,3 +14,4 @@ router.include_router(_af_tt_router)
 router.include_router(_af_excel_router)
 router.include_router(_admin_html_router)    # NEW
 router.include_router(_admin_logic_router)   # NEW
+router.include_router(_cleanup_validator_router)                             # NEW
