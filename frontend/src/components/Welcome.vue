4de6757b0df8789d068e241b49b1ef2950622505
Healthopilot <davidkych.med@gmail.com>
2025-07-19 12:25:59 +0800

    chore: remove legacy frontend (pre-Admin One reset)

--- frontend/src/components/Welcome.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="min-h-screen flex flex-col items-center justify-center">
-    <h1 class="text-4xl font-bold mb-4">Welcome to Cursus</h1>
-    <p class="text-lg">
-      Your marathon-planning companion is coming soon. 🏃‍♂️
-    </p>
-  </div>
-</template>
