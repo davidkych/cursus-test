8957a2b0993eb872de274a2997cb2d0bfba2c5e2
Healthopilot <davidkych.med@gmail.com>
2025-08-12 10:40:23 +0800

    code redemption system

--- src/routers/auth/endpoints.py
+++ src/routers/auth/endpoints.py
@@ -8,8 +8,10 @@ from fastapi import APIRouter
 from .register import router as register_router
 from .login    import router as login_router
 from .me       import router as me_router
+from .codes import router as codes_router
 
 router = APIRouter()
 router.include_router(register_router)
 router.include_router(login_router)
 router.include_router(me_router)
+router.include_router(codes_router)
